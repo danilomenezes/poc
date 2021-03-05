c5ae184c85ba90feb563f03a596dcb12e6381fd4
Danilo Menezes <danilo.mnz@gmail.com>
2021-03-04 21:45:20 -0300

    initial commit

---
+++ src/test/java/com/mnz/poc/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnz.poc.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
